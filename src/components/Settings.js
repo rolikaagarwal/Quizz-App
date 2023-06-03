19f1231b5280888e239eaeb1ecac8be8c58c41d2
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-06-03 09:39:54 +0530

    added more categories

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react";
 const Setting = () => {
   const [options, setOptions] = useState(null);
   const [selectCategory, setSelectCategory] = useState(" ");
-  const[difficulty, setDifficulty]= useState(" ");
+  const [difficulty, setDifficulty] = useState(" ");
+  const [questionType, setquestionType] = useState(" ");
+  const [numberofQuestions, setnumberofQuestions] = useState(" ");
+  //   console.log(numberofQuestions)
+
   useEffect(() => {
     const apiUrl = "https://opentdb.com/api_category.php";
     fetch(apiUrl)
@@ -16,7 +20,8 @@ const Setting = () => {
     <>
       <div className="category">
         <h2>Select Category</h2>
-        <select className="options-category"
+        <select
+          className="options-category"
           value={selectCategory}
           onChange={(e) => setSelectCategory(e.target.value)}
         >
@@ -31,11 +36,37 @@ const Setting = () => {
       </div>
       <div className="category">
         <h2>Difficulty Level</h2>
-        <select className="options-category" value={difficulty} onChange={(e)=> setDifficulty(e.target.value)}>
-            <option>Easy</option>
-            <option>Meduim</option>
-            <option>Hard</option>
+        <select
+          className="options-category"
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+        >
+          <option>All</option>
+          <option>Easy</option>
+          <option>Meduim</option>
+          <option>Hard</option>
         </select>
+      </div>
+      <div className="category">
+        <h2>Question Type</h2>
+        <select
+          className="options-category"
+          value={questionType}
+          onChange={(e) => setquestionType(e.target.value)}
+        >
+          <option>All</option>
+          <option>Multiple Choice Question</option>
+          <option>True/False</option>
+        </select>
+      </div>
+      <div className="category">
+        <h2>Number of Questions</h2>
+        <input
+          type="text"
+          className="options-category"
+          value={numberofQuestions}
+          onChange={(e) => setnumberofQuestions(e.target.value)}
+        />
       </div>
     </>
   );
